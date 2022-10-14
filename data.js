42abe3497345693c59610c930f1ff004b0b0e26d
aarchishahh <aarchishahh@gmail.com>
2022-10-14 17:55:43 +0530

    aarchi-branch

--- data.js
+++ data.js
@@ -15,7 +15,21 @@ const data = [
   // ======================== <Your name> Profile ENDS ==========================
 
   // Copy the above template and paste it below this line
+  {
+    id: 0,
+    name: "aarchi ",
+    branch: "CS",
+    introduction: "hi ",
+    skills: ["add your skills", "skill1", "skill2"],
+    socialsHandles: {
+      github: "add your github handle",
+      twitter: "add your twitter handle",
+      linkedin: "add your linkedin handle",
+    },
+  },
 ]
+
+
 
 // To be used later
 export default data
